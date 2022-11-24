71c0c78b7016bcd005f0416d7d7328c7ab09ea41
Leo9723 <pala9761@gmail.com>
2022-11-24 14:09:15 +0100

    aggiunta possibilità di generare password in base ai dati inseriti dall'utente

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let nome = prompt('inserisci il tuo nome');
+
+let cognome = prompt('inserisci il tuo cognome');
+
+let colore = prompt('inserisci il tuo colore preferito');
+
+document.getElementById('password').innerHTML = 'La tua password &egrave;: ' + nome + cognome + colore + 21
